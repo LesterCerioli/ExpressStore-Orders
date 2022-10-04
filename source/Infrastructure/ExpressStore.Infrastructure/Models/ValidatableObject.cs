8e919b1cf935e5036837ea02142398d59aea46fa
unknown <lesterlucasit@hotmail.com>
2022-10-04 18:36:14 -0300

    Initial repository

---
+++ source/Infrastructure/ExpressStore.Infrastructure/Models/ValidatableObject.cs
@@ -0,0 +1,7 @@
+namespace ExpressStore.Infrastructure.Models
+{
+    public class ValidatableObject
+    {
+        
+    }
+}
---
+++ test/ExpressStore.Modules.Orders.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Xunit;
+
+namespace ExpressStore.Modules.Orders.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
